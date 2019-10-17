be77a348a73c9961d9a21046a978a3eaff6f81ba
Berow <52027524+Berow@users.noreply.github.com>
2019-10-17 20:42:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function createCard(data) {
     let dd = String(today.getDate()).padStart(2, '0');
     let mm = String(today.getMonth() + 1).padStart(2, '0');
     let yyyy = today.getFullYear();
-    today = dd + '/' + dd + '/' + yyyy;
+    today = dd + '/' + mm + '/' + yyyy;
 
     let tileParent = document.createElement('div');
     tileParent.classList.add('tile', 'is-12', 'is-parent');
